4afc640ffbe3b64bc96004bef8deaf73ff4ea4f2
antoinedoolaeghe <antoinedoolaeghe@gmail.com>
2017-07-25 17:00:39 +0100

    add model link.rb

--- Gemfile
+++ Gemfile
@@ -3,10 +3,13 @@ source "https://rubygems.org"
 
 ruby '2.4.0'
 
+gem 'data_mapper'
+gem 'dm-postgres-adapter'
 gem 'rake'
 gem 'sinatra'
 
 group :test do
+  gem 'pry'
   gem 'capybara'
   gem 'rspec'
   gem 'rubocop'
--- spec/features/view_list_spec.rb
+++ spec/features/view_list_spec.rb
@@ -4,7 +4,7 @@ feature "view the list of bookmarks" do
     visit('/links')
     expect(page.status_code).to eq 200
     within 'ul#links' do
-    expect(page). to have_content "Makers Academy"
+    expect(page).to have_content "Makers Academy"
   end
   end
 end
